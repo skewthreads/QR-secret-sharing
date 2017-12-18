d010216c076184d8538258354ea18d536a82e0c9
Thanos Giannopoulos <thanosgn@gmail.com>
2017-12-18 19:56:29 +0200

    Add parties, threshold

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import png
 
 def main():
     # Secret-share the message using Shamir's secret sharing scheme.
-    shares = PlaintextToHexSecretSharer.split_secret("correct horse battery staple", 2, 3)
+    parties = 3
+    threshold = parties - 1
+    shares = PlaintextToHexSecretSharer.split_secret("correct horse battery staple", threshold, parties)
     print(shares)
     for share in shares: # Create png for each share
         img = pyqrcode.create(share)
         img.png(share[0]+'.png', scale = 8)
 
     # Recover
-    # print(PlaintextToHexSecretSharer.recover_secret(shares[0:2]))
+    # print(PlaintextToHexSecretSharer.recover_secret(shares[0:threshold]))
 
 if __name__ == "__main__":
 	main()
