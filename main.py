c703cc976d7549460eb0bd673a9a156c434925bd
Dimitris Mouris <jimouris@gmail.com>
2017-12-18 20:22:34 +0200

    Add inquirer to parse arguments

--- main.py
+++ main.py
@@ -1,19 +1,64 @@
 from secretsharing import PlaintextToHexSecretSharer
 import pyqrcode
 import png
+import inquirer
 
 def main():
+    # Select number of shares
+    questions = [
+        inquirer.List('parties',
+                    message='How many shares do you want?',
+                    choices=['2', '3', '4', 'other'],
+                ),
+    ]
+    answer = inquirer.prompt(questions)
+    if answer['parties'] == 'other':
+        parties = int(raw_input('Type a number: '))
+        while parties < 2:
+            parties = int(raw_input('Type a number greater than 1: '))
+    else:
+        parties = int(answer['parties'])
+    threshold = parties
+
+    # Select type of shares output
+    questions = [
+        inquirer.List('format',
+                    message='Select the format of output images',
+                    choices=['png', 'svg', 'terminal'],
+                ),
+    ]
+    format = inquirer.prompt(questions)['format']
+
+    # Select size of shares output
+    if format != 'terminal':
+        questions = [
+            inquirer.List('scale',
+                        message='Size of output images',
+                        choices=['Small', 'Medium', 'Large'],
+                    ),
+        ]
+        answers = inquirer.prompt(questions)
+        if answers['scale'] == 'Small':
+            scale = 2
+        elif answers['scale'] == 'Medium':
+            scale = 4
+        elif answers['scale'] == 'Large':
+            scale = 8
+
     # Secret-share the message using Shamir's secret sharing scheme.
-    parties = 3
-    threshold = parties - 1
-    shares = PlaintextToHexSecretSharer.split_secret("correct horse battery staple", threshold, parties)
+    shares = PlaintextToHexSecretSharer.split_secret('correct horse battery staple', threshold, parties)
     print(shares)
     for share in shares: # Create png for each share
         img = pyqrcode.create(share)
-        img.png(share[0]+'.png', scale = 8)
+        if format == 'png':
+            img.png(share[0]+'.png', scale = scale)
+        elif format == 'svg':
+            img.svg(share[0]+'.svg', scale = scale)
+        elif format == 'terminal':
+            print(img.terminal())
 
     # Recover
     # print(PlaintextToHexSecretSharer.recover_secret(shares[0:threshold]))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
